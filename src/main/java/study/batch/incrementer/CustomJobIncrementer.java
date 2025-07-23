6aebd699ce94f74062904ea735b46befa7207b1a
Chan <ieejo716@naver.com>
2025-07-23 16:30:28 +0900

    JobIncrementer 구현

---
+++ src/main/java/study/batch/incrementer/CustomJobIncrementer.java
@@ -0,0 +1,21 @@
+package study.batch.incrementer;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersIncrementer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CustomJobIncrementer implements JobParametersIncrementer {
+
+    static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+
+    @Override
+    public JobParameters getNext(JobParameters parameters) {
+        String id = format.format(new Date());
+        return new JobParametersBuilder()
+                .addString("run.id", id)
+                .toJobParameters();
+    }
+}
